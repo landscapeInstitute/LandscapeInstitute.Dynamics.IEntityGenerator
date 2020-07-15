092597c76d0de2be5bc41a46361798f5865e23a1
Louis Varley <louis.varley@landscapeinstitute.org>
2020-07-15 16:57:02 +0100

    Initial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LandscapeInstitute.Dynamics.IEntityGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public IOrganizationService GetOrganiszationService()
+        {
+
+            var connectionString = @"AuthType = Office365; Url = https://landscapeinstituteuat.crm11.dynamics.com/;Username=felinesoft@landscapeinstitute.org;Password=PantsLandscapeinstitute007";
+            CrmServiceClient conn = new CrmServiceClient(connectionString);
+
+            IOrganizationService service;
+            service = (IOrganizationService)conn.OrganizationWebProxyClient != null ? (IOrganizationService)conn.OrganizationWebProxyClient : (IOrganizationService)conn.OrganizationServiceProxy;
+
+            return service;
+
+        }
+    }
+}
